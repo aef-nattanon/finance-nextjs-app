c7fa6c3aaace52d5ba7cf46ecc9c3a3e8a902564
aefnattanon <aef.nattanon@gmail.com>
2023-03-08 21:59:54 +0700

    Update modal

--- src/components/NewTransitionModal.jsx
+++ src/components/NewTransitionModal.jsx
@@ -1,52 +1,72 @@
-import { Modal } from 'antd';
-import { useState } from 'react';
-export default function NewTransitionModal() {
+import { Modal, Form, Input, Radio, InputNumber } from 'antd';
+import { useState, useRef } from 'react';
+export default function NewTransitionModal({ loading = false, onFinish = () => { } }) {
+  const [form] = Form.useForm();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
   const handleOk = () => {
     setIsModalOpen(false);
+    onReset()
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    onReset()
+  };
+  const onReset = () => {
+    form.resetFields();
   };
   return (
     <>
       <button class="btn" onClick={showModal}>New Transaction</button>
       <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={null} maskClosable={false} closable={false}>
-        <div class="modal">
-          <div class="modal-title">Add New Transaction</div>
-          <form>
-            <section class="modal-input">
-              <div class="input-group">
-                <label>Transaction Name</label>
-                <input type="text" />
-              </div>
-              <div class="input-group">
-                <label>Transaction Type</label>
-                <div class="radio-group">
-                  <label>
-                    <input type="radio" id="html" name="fav_language" value="HTML" />
-                    <span>Income</span>
-                  </label>
-                  <label>
-                    <input type="radio" id="html" name="fav_language" value="HTML" />
-                    <span>Expenses</span>
-                  </label>
-                </div>
-              </div>
-              <div class="input-group">
-                <label>Amount</label>
-                <input type="text" />
-              </div>
-            </section>
+        <div class="modal-title">Add New Transaction</div>
+        <Form
+          form={form}
+          labelWrap
+          onFinish={onFinish}>
+          <Form.Item
+            label="Transaction Name"
+            name="title"
+            required>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Transaction Type"
+            name="type"
+            required>
+            <Radio.Group
+              optionType="button"
+              buttonStyle="solid">
+              <Radio.Button value="income">Income</Radio.Button>
+              <Radio.Button value="expend">Expenses</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="Amount"
+            name="amount"
+            required>
+            <InputNumber />
+          </Form.Item>
+          <Form.Item>
             <section class="modal-action">
-              <button type="button" class="btn" onClick={handleCancel}>Cancel</button>
-              <button class="btn btn--primary">Add</button>
+              <button
+                onClick={handleCancel}
+                loading={loading}
+                class="btn"
+                htmlType="button">
+                Cancel
+              </button>
+              <button
+                loading={loading}
+                class="btn btn--primary"
+                htmlType="submit">
+                Add
+              </button>
             </section>
-          </form>
-        </div>
+          </Form.Item>
+        </Form>
       </Modal >
     </>
   );
